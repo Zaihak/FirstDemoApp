a6d0572a182338695a71541b72b48d98fc5b8a0f
zaihak <hankomin13@gmail.com>
2018-11-15 18:39:39 +0630

    poloroRotolo library part-2

--- app/src/main/java/net/myanmarlink/smartcitydemo/AppIntroActivity.kt
+++ app/src/main/java/net/myanmarlink/smartcitydemo/AppIntroActivity.kt
@@ -1,15 +1,23 @@
 package net.myanmarlink.smartcitydemo
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v4.app.Fragment
+import android.view.View
+import android.view.WindowManager
 import com.github.paolorotolo.appintro.AppIntro
 import ui.fragment.SampleSlide
 
 class AppIntroActivity:AppIntro() {
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+                 this.makeFullScreen()
+        this.changeStatusBarColor()
+
         addSlide(SampleSlide.newInstance(R.layout.view_intro_layout_one))
         addSlide(SampleSlide.newInstance(R.layout.view_intro_layout_two))
         addSlide(SampleSlide.newInstance(R.layout.view_intro_layout_three))
@@ -24,4 +32,25 @@ class AppIntroActivity:AppIntro() {
         super.onDonePressed(currentFragment)
         finish()
     }
+    /**
+     * Making full screen
+     */
+    private fun makeFullScreen() {
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        }
+    }
+    /**
+     * Making notification bar transparent
+     */
+    private fun changeStatusBarColor() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            val window = window
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            window.statusBarColor = Color.TRANSPARENT
+        }
+    }
+
 }
